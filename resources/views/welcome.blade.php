a0114590ec814bc0075d4a515a9339cb13cc07d5
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-14 21:42:44 -0500

    New Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,23 +73,9 @@
                         <div class="vertical-middle subscribe-widget" data-loader="button">
                             <div class="col-padding">
                                 <div class="heading-block nobottomborder bottommargin-sm">
-                                    <h1 style="line-height: 1.4;font-size: 24px;">Stay in Touch to get Exclusive Offers &amp; Daily Digest</h1>
-                                    <span style="font-size:16px; top-margin: 20px;" class="t300">Deals, Coupon Codes &amp; Insider Tips to get the Best out of our Products &amp; Services</span>
+                                    <h1 style="line-height: 1.4;font-size: 24px;">This page is about our experience with Ingram Plumbing in League City, Texas</h1>
+                                    <span style="font-size:16px; top-margin: 20px;" class="t300">This page describes how Ingram Plumbing mislead us as to the work they would perform, grossly overcharged us, failed to get permits and inspections, did poor work, overbilled hours, and had generally poor behavior.</span>
                                 </div>
-                                <div class="widget-subscribe-form-result"></div>
-                                <form id="slider-subscribe-form" action="../include/subscribe.php" method="post" class="nobottommargin">
-                                    <div class="col_half">
-                                        <input type="text" class="sm-form-control required" name="widget-subscribe-form-fname" placeholder="First Name">
-                                    </div>
-                                    <div class="col_half col_last">
-                                        <input type="text" class="sm-form-control required" name="widget-subscribe-form-lname" placeholder="Last Name">
-                                    </div>
-                                    <div class="clear"></div>
-                                    <div class="col_full">
-                                        <input type="email" class="sm-form-control required" name="widget-subscribe-form-email" placeholder="Enter Email Address">
-                                    </div>
-                                    <button type="submit" class="button button-dark button-3d button-rounded btn-block nomargin" value="submit"><i class="icon-email2"></i> Subscribe Now</button>
-                                </form>
                             </div>
                         </div>
                         <a href="#" data-scrollto="#section-about" data-easing="easeInOutExpo" data-speed="1250" data-offset="65" class="one-page-arrow"><i class="icon-angle-down infinite animated fadeInDown"></i></a>
